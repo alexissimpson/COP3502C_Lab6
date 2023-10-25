e731d4112b548ad89dd95ceea60dcd2287f45b50
Maria Davis <davismaria@ufl.edu>
2023-10-25 18:47:29 -0400

    added decoder

--- main.py
+++ main.py
@@ -10,6 +10,14 @@ def encode(password):
 
     return encoded_password
 
+def decoder(password):
+    new_p = ""
+    
+    for digit in password:
+        new_d = str((int(digit) - 3) % 10)
+        new_p += new_d
+    return new_p
+
 
 def main():
     # Prints Menu
